d2f979501f7cbed06303aaee81e7b2eb41164f9c
Guoxi <liuguoxi@chd.edu.cn>
2019-03-14 19:58:04 -0400

    Change the parameters for autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -9,7 +9,7 @@ print(input_feature.shape)
 
 # Set parameters for learning
 learning_rate = 0.01
-num_steps = 100
+num_steps = 200
 batch_size = 16 
 
 # Set parameters for display
@@ -17,51 +17,37 @@ display_step = 10
 
 # Set parameters for network
 num_input = 524
-num_hidden_1 = 400
-num_hidden_2 = 300
-num_hidden_3 = 200
-num_hidden_4 = 100
+num_hidden_1 = 256
+num_hidden_2 = 128
 
-X = tf.placeholder("float", [None, num_input])
+X = tf.placeholder("float", [None, num_input], name="input")
 
 # Hidden layer settings
 weights = {
-    "encoder_h1": tf.Variable(tf.random_normal([num_input, num_hidden_1])),
-    "encoder_h2": tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2])),
-    "encoder_h3": tf.Variable(tf.random_normal([num_hidden_2, num_hidden_3])),
-    "encoder_h4": tf.Variable(tf.random_normal([num_hidden_3, num_hidden_4])),
-    "decoder_h1": tf.Variable(tf.random_normal([num_hidden_4, num_hidden_3])),
-    "decoder_h2": tf.Variable(tf.random_normal([num_hidden_3, num_hidden_2])),
-    "decoder_h3": tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1])),
-    "decoder_h4": tf.Variable(tf.random_normal([num_hidden_1, num_input])),
+    "encoder_h1": tf.Variable(tf.random_normal([num_input, num_hidden_1]), name="encoder_weights1"),
+    "encoder_h2": tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2]), name="encoder_weights2"),
+    "decoder_h1": tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1]), name="decoder_weights1"),
+    "decoder_h2": tf.Variable(tf.random_normal([num_hidden_1, num_input]), name="decoder_weights2"),
 }
 
 biases = {
-    "encoder_b1": tf.Variable(tf.random_normal([num_hidden_1])),
-    "encoder_b2": tf.Variable(tf.random_normal([num_hidden_2])),
-    "encoder_b3": tf.Variable(tf.random_normal([num_hidden_3])),
-    "encoder_b4": tf.Variable(tf.random_normal([num_hidden_4])),
-    "decoder_b1": tf.Variable(tf.random_normal([num_hidden_3])),
-    "decoder_b2": tf.Variable(tf.random_normal([num_hidden_2])),
-    "decoder_b3": tf.Variable(tf.random_normal([num_hidden_1])),
-    "decoder_b4": tf.Variable(tf.random_normal([num_input])),
+    "encoder_b1": tf.Variable(tf.random_normal([num_hidden_1]), name="encoder_biases1"),
+    "encoder_b2": tf.Variable(tf.random_normal([num_hidden_2]), name="encoder_biases2"),
+    "decoder_b1": tf.Variable(tf.random_normal([num_hidden_1]), name="decoder_biases1"),
+    "decoder_b2": tf.Variable(tf.random_normal([num_input]), name="decoder_biases2"),
 }
 
 # Building the encoder
 def encoder(x):
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
-    layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['encoder_h3']), biases['encoder_b3']))
-    layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(layer_3, weights['encoder_h4']), biases['encoder_b4']))
-    return layer_4
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']), name="encoder_layer1")
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']), name="encoder_layer2")
+    return layer_2
 
 # Building the decoder
 def decoder(x):
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
-    layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['decoder_h3']), biases['decoder_b3']))
-    layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(layer_3, weights['decoder_h4']), biases['decoder_b4']))
-    return layer_4
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']), name="decoder_layer1")
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']), name="decoder_layer2")
+    return layer_2
 
 # Construct the model
 encoder_op = encoder(X)
@@ -70,13 +56,14 @@ decoder_op = decoder(encoder_op)
 y_pre = decoder_op
 y = X
 
-loss = tf.reduce_mean(tf.pow(y - y_pre, 2))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+loss = tf.reduce_mean(tf.pow(y - y_pre, 2), name="mean_square_loss")
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name="optimizer").minimize(loss)
 
 init = tf.global_variables_initializer()
 
 # Run network model, start traning process
 with tf.Session() as sess:
+    writer = tf.summary.FileWriter("output", sess.graph)
     sess.run(init)
     total_batch = int(input_feature.shape[0]/batch_size)
 
