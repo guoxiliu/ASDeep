858b9fce2fd2d91af92d10cf4084d989aa027b8c
Guoxi <liuguoxi@chd.edu.cn>
2019-04-07 23:38:57 -0400

    Use k-mer for transcript dataset

---
+++ k-mer.py
@@ -0,0 +1,83 @@
+gene_list = ['A', 'C', 'G', 'T']
+
+#################################################
+# Helper functions define here
+#################################################
+
+def backtracking(k, comb, res):
+    """
+    Use backtracking algorithm to find all possible combinations of length k.
+    
+    Parameters
+    ---------------
+    k: int
+        Length of combination.
+    comb: string
+        Current combination.
+    res: list
+        The list that contains all possible combinations.
+
+    Returns
+    ---------------
+    None
+    """
+    if k == 0:
+        res.append(comb)
+        return
+
+    for gene in gene_list:
+        comb += gene
+        backtracking(k-1, comb, res)
+        comb = comb[:-1]
+
+
+def get_k_mer_list(k):
+    """
+    Get the k-mer list with a given k.
+
+    Parameters
+    ---------------
+    k: int
+        Length of combination.
+
+    Returns
+    ---------------
+    res: list
+        The list that contains all possible combinations of length k.
+    """
+    res = []
+    backtracking(k, "", res)
+    return res
+
+#################################################
+# Extract k-mer features
+#################################################
+
+# variables related to files
+data_path = "Dataset/"
+feature_path = "Features/"
+data_file = "ASD_transcript_sens.csv"
+feature_file = "ASD_transcript_K_mer.feature"
+
+# get 4-mer list
+k_mer_list = get_k_mer_list(4)
+k_mer_dict = {}
+for i in range(len(k_mer_list)):
+    k_mer_dict[k_mer_list[i]] = i
+
+# translate the gene transcript to k-mer features
+k_mer_features = []
+with open(data_path + data_file, "r") as infile:
+    for line in infile:
+        words = line.split()
+        feature = [0] * len(k_mer_list)
+        for word in words:
+            feature[k_mer_dict[word]] += 1
+        k_mer_features.append(feature)
+
+# save k-mer features to file
+with open(feature_path + feature_file, "w+") as outfile:
+    for feature in k_mer_features:
+        for item in feature:
+            outfile.write("%d " % item)
+        outfile.write("\n")
